221b746bd7a1978c91eaffae3de5f114e2f94a47
bettercallaashutosh <bettercallaashutosh@gmail.com>
2026-06-25 20:53:40 +0530

    Update 14.py

--- ARRAY/problems/14.py
+++ ARRAY/problems/14.py
@@ -4,22 +4,15 @@ arr = [-2,5,-3,8,-1,2]
 
 size = 6
 
+current_sum = 0
 max_sum = 0 
 
-def addition(array):
-    total = 0
-    for i in range(len(array)):
-        total += array[i]
-
-    return total
-
 for i in range(size):
-    for j in range(i+1,size+1):
-        if  (addition(arr[i:j])) > max_sum:
-            max_sum = addition(arr[i:j])
-
+    current_sum = current_sum + arr[i]
+    if current_sum > max_sum :
+        max_sum = current_sum
+    if current_sum < 0:
+        current_sum = 0
 
 print(max_sum)
-
-
 
